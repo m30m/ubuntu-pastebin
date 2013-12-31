5beed56b2007d5559f6cebdbfdd8fa4aa04b4fb5
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamV@gmail.com>
2013-12-31 19:17:53 +0330

    First Version of extension that is really working

---
+++ scr.js
@@ -0,0 +1,32 @@
+function selectText(element) {
+    var doc = document
+        , text = $('div.paste > pre')[0]
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) { //ms
+        range = doc.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) { //all others
+        selection = window.getSelection();        
+        range = doc.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+$(document).ready(function() {
+     
+        $(document).keydown(function (e){
+            var s = '';
+            if (e.ctrlKey){
+				if(e.which==65){
+					selectText();
+					e.preventDefault();
+				}
+			}
+            //s+= "Key : " + e.which;
+            //$("#test").html(s);         
+            
+        });
+});
